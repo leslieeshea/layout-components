f62d7d560ce1aa1a8ae93570b37755af40ecb1d7
Leslie Shea <leslieyshea@gmail.com>
2019-05-13 13:13:35 -0700

    added list of dogs and css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('hi there');
+import styles from './index.css';
+const root = document.getElementById('root');
+
+const dogs = ['spot', 'rover', 'fido', 'buddy'];
+
+const ul = document.createElement('ul');
+ul.className = styles.dogs;
+root.appendChild(ul);
+
+dogs.forEach(dog => {
+  const li = document.createElement('li');
+  li.textContent = dog;
+  ul.appendChild(li);
+});
